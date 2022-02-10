2c674431291fbdde4fe85d65e8318c655f8cf408
ItayElf <thh123321@gmail.com>
2022-02-10 13:10:04 +0200

    fix home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
+    <div className="h-full w-full">
       <Header username="" />
       <Home />
       <Footer />
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,22 +2,22 @@ import code from "../imgs/code.jpg";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-between">
-      <main className="mx-auto mt-10 max-w-7xl px-4 ">
+    <div className="h-full-main grid grid-cols-2 items-center">
+      <main className="mx-auto mt-10 max-w-7xl px-4">
         <div>
-          <h1 className="text-6xl font-extrabold tracking-tight">
+          <h1 className="text-8xl font-extrabold tracking-tight">
             <span className="block ">Explore and share</span>
             <span className="text-primary block">programming project</span>
           </h1>
-          <p className="text-primary-dark mx-0 mt-5 max-w-xl text-xl">
-            Taj is a platform where developers can share their projects and get
+          <p className="text-primary-dark mx-0 mt-5 max-w-xl text-2xl">
+            Taj is a platform for developers to share their projects and get
             feedback from other developers.
           </p>
-          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+          <div className="mt-8 flex justify-start">
             <div className="rounded-md shadow">
               <a
                 href="#"
-                className="bg-primary hover:bg-primary/80 flex w-full items-center justify-center rounded-md border border-transparent py-4 px-10 text-lg font-medium text-white"
+                className="bg-primary hover:bg-primary/80 flex w-full items-center justify-center rounded-md border border-transparent py-4 px-10 text-xl font-medium text-white"
               >
                 Get started
               </a>
@@ -26,7 +26,7 @@ export default function Home() {
             <div className="mt-3 sm:mt-0 sm:ml-3">
               <a
                 href="#"
-                className="bg-primary-extra-light text-primary hover:bg-primary-extra-light/80 flex w-full items-center justify-center rounded-md border border-transparent py-4 px-10 text-lg font-medium"
+                className="bg-primary-extra-light text-primary hover:bg-primary-extra-light/80 flex w-full items-center justify-center rounded-md border border-transparent py-4 px-10 text-xl font-medium"
               >
                 Learn More
               </a>
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <img src={code} className="clip-path-slash object-cover" />
+      <img src={code} className="clip-path-slash h-full object-cover" />
     </div>
   );
 }
